641b2709b7ccc2138695be42cc4272e236a8792a
125054306-mtt <125054306@upq.edu.mx>
2026-09-21 16:39:53 -0600

    Cincuerizado de 15 elemntos

--- Arreglos.py
+++ Arreglos.py
@@ -1,4 +1,4 @@
-#Declarando un arreglo
+""" #Declarando un arreglo
 numeros = [10, 20, 30, 40, 50]
 
 #Imprimo un elemento espe. del arreglo
@@ -24,5 +24,40 @@ frutas = ["Manzana", "Fresa", "Sandia", "Mango", "Melon", "Platano"]
 frutas.pop(4)
 print(frutas)
 
+#Eliminamos un elemento del arreglo usando el nombre
 frutas.remove("Manzana")
 print(frutas)
+
+#Declaración de un arreglo vacio
+arreglo = []
+print(arreglo)
+
+n = int(input("Ingrese el tamaño el arreglo: "))
+print(arreglo)
+
+for i in range(n):
+    dato = int(input("Ingrese un nmero: "))
+    arreglo.append(dato)
+
+print("El arreglo es:", arreglo)
+
+n = int(input("Ingrese el tamaño del arreglo: "))
+
+arreglo = [0] * n
+
+for i in range(n):
+        dato = int(input("Ingrese un número"))
+        arreglo[i] = dato
+
+print(arreglo)
+"""
+original = []
+print("Introduce 15 números enteros (entre 0 y 500):")
+for i in range(15):
+    num = int(input(f"Número {i + 1}: "))
+    original.append(num)
+
+cincuerizado = [num if num % 5 == 0 else num + (5 - (num % 5)) for num in original]
+
+print("\nArray original:    ", *original)
+print("Array cincuerizado:", *cincuerizado)
